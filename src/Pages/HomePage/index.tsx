0df4443ac273d751b8281bf1eaf7fe45348b809f
Blindeen <kuba.szmajda@gmail.com>
2023-05-15 20:54:21 +0200

    add homepage component

---
+++ src/Pages/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import Button from 'Components/Button';
+import Input from 'Components/Input';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button shape="default">dsadsasadasdsada</Button>
+      <Input placeholder="123" />
+    </div>
+  );
+};
+
+export default HomePage;
