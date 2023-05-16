c15e00a08c127ddf2d6ed45df37582eec1b23173
Blindeen <kuba.szmajda@gmail.com>
2023-05-16 08:38:42 +0200

    remove unnecessary `return` statement

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from 'antd';
 import { StyledButton } from 'Components/Button/styles';
 
 const Button = ({ type = 'primary', shape = 'round', ...rest }: ButtonProps) => {
-  return <StyledButton type={type} shape={shape} {...rest} />;
+  <StyledButton type={type} shape={shape} {...rest} />;
 };
 
 export default Button;
--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -3,7 +3,7 @@ import { InputProps } from 'antd';
 import { StyledInput } from 'Components/Input/styles';
 
 const Input = ({ type = 'text', ...rest }: InputProps) => {
-  return <StyledInput type={type} {...rest} />;
+  <StyledInput type={type} {...rest} />;
 };
 
 export default Input;
