72c4fd5a4b14390e9245d8ec5fc96b2a12d3e650
VibKkk <240785@edu.p.lodz.pl>
2023-05-16 19:30:48 +0200

    create Landing Page

---
+++ src/Pages/LandingPage/index.tsx
@@ -0,0 +1,10 @@
+import Background from 'Components/Background';
+import SessionChoice from 'Containers/SessionChoice';
+
+const LandingPage = () => (
+  <Background imageUrl="/Arts/HomePage.jpg">
+    <SessionChoice />
+  </Background>
+);
+
+export default LandingPage;
