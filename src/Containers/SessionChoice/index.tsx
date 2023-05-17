e375abbaf87643421511d689fe1f0ddfb253c467
VibKkk <240785@edu.p.lodz.pl>
2023-05-17 15:35:47 +0200

    Add fetch

--- src/Containers/SessionChoice/index.tsx
+++ src/Containers/SessionChoice/index.tsx
@@ -5,11 +5,34 @@ import Button from 'Components/Button';
 import Input from 'Components/Input';
 import { Box, CenteredContainer, OptionalText, Tags } from './styles';
 
-const SessionChoice = () => {
-  const newSessionButton: React.ReactNode = <Button>Create new session</Button>;
-  const joinSessionButton: React.ReactNode = <Button>Join session</Button>;
+interface Payload {
+  nickname: string;
+  game_code: string;
+}
 
-  const [userInput, setUserInput] = useState<string>('');
+const SessionChoice = () => {
+  const [nickname, setNickname] = useState<string>('');
+  const [gameCode, setGameCode] = useState<string>('');
+
+  const newSessionButton = 'Create new session';
+  const joinSessionButton = 'Join session';
+
+  const onSubmit = async () => {
+    const info: Payload = { nickname, game_code: gameCode };
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/v1/game/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(info),
+      });
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <Box>
@@ -19,15 +42,17 @@ const SessionChoice = () => {
             <Row gutter={[0, 30]}>
               <Col span={24}>
                 <Tags>Nickname</Tags>
-                <Input maxLength={12}></Input>
+                <Input maxLength={12} onChange={(e) => setNickname(e.target.value)}></Input>
               </Col>
               <Col span={24}>
                 <Tags>
                   Game Code <OptionalText>(Optional)</OptionalText>
                 </Tags>
-                <Input maxLength={6} onChange={(e) => setUserInput(e.target.value)}></Input>
+                <Input maxLength={6} onChange={(e) => setGameCode(e.target.value)}></Input>
               </Col>
-              <Col span={24}>{userInput === '' ? newSessionButton : joinSessionButton}</Col>
+              <Col span={24}>
+                <Button onClick={onSubmit}>{gameCode === '' ? newSessionButton : joinSessionButton}</Button>
+              </Col>
             </Row>
           </Col>
         </Row>
