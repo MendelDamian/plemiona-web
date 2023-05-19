1997c322cdd4fa34033e8c23a65d0b8fde7bb235
VibKkk <240785@edu.p.lodz.pl>
2023-05-19 18:17:37 +0200

    create lobby page

---
+++ src/Pages/LobbyPage/index.tsx
@@ -0,0 +1,10 @@
+import ImageHero from 'Components/ImageHero';
+import Lobby from 'Containers/Lobby';
+
+const LobbyPage = () => (
+  <ImageHero imageurl="/Arts/HomePage.jpg">
+    <Lobby />
+  </ImageHero>
+);
+
+export default LobbyPage;
