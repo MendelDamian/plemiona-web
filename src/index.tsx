09c1c2a858585b56faf6cbb15d4411eaad70f2ce
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-05-23 15:51:34 +0200

    add routes for landing and lobby pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider,
-} from "react-router-dom";
-
-import './index.css';
-import {router} from "./router";
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
 
