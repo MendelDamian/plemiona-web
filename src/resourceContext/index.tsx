ed8a539c2965bbb0053969d34c6bda20191b5264
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-05-23 18:25:37 +0200

    add resources Context

---
+++ src/resourceContext/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+type resourcesType = {
+  wood:number,
+  clay:number,
+  steel:number,
+}
+
+const initialResources = {
+  wood:0,
+  clay:0,
+  steel:0,
+}
+
+type resourcesContextType = {
+  resources:resourcesType,
+  setResources:(resources:resourcesType) => void
+}
+
+export const Resources = React.createContext<resourcesContextType>({resources:initialResources, setResources:null});
+
+export const ResourcesProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [resources, setResources] = useState(initialResources)
+  return <Resources.Provider value={{resources, setResources}}>{children}</Resources.Provider>;
+};
