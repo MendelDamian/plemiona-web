b2463f674f25dabc305e7aa708c1e911b101e481
VibKkk <240785@edu.p.lodz.pl>
2023-05-29 21:38:41 +0200

    link music and cursor images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from 'router';
 import 'App.css';
+import { RouterProvider } from 'react-router-dom';
+import { useEffect } from 'react';
+import { router } from 'router';
 
-const App = () => <RouterProvider router={router} />;
+const App = () => {
+  useEffect(() => {
+    const audio = document.querySelector('audio');
+    document.addEventListener(
+      'click',
+      () => {
+        if (audio) {
+          audio.play();
+        }
+      },
+      { once: true }
+    );
+  }, []);
+
+  return (
+    <>
+      <audio autoPlay loop>
+        <source src="/Assets/Music/Waterflame%20-%20Glorious%20Morning%20Extended.mp3" type="audio/mpeg" />
+      </audio>
+      <RouterProvider router={router} />
+    </>
+  );
+};
 
 export default App;
