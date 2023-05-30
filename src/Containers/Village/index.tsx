e36a26e8c1471f102ead4fa0f0a5c8b7e913c1cf
VibKkk <240785@edu.p.lodz.pl>
2023-05-30 21:42:54 +0200

    make village container

--- src/Containers/Village/index.tsx
+++ src/Containers/Village/index.tsx
@@ -1,16 +1,95 @@
-import { BuildingLvL, Container, Warehouse } from 'Containers/Village/styles';
-import { useState } from 'react';
+import { Container } from 'Containers/Village/styles';
+import Building, { BuildingProps } from 'Components/Building';
 
 const Village = () => {
-  const [warehouseLvl, setWarehouseLvl] = useState(1);
+  const Buildings: BuildingProps[] = [
+    {
+      name: 'Tartak',
+      tier: 1,
+      lvl: 1,
+      posX: 550,
+      posY: 480,
+      sizeX: 200,
+      sizeY: 200,
+      posLvlX: 170,
+      posLvlY: 20,
+    },
+    {
+      name: 'Spichlerz',
+      tier: 1,
+      lvl: 1,
+      posX: 280,
+      posY: 150,
+      sizeX: 200,
+      sizeY: 200,
+    },
+    {
+      name: 'Koszary',
+      tier: 1,
+      lvl: 1,
+      posX: 10,
+      posY: 400,
+      sizeX: 200,
+      sizeY: 200,
+    },
+    {
+      name: 'Cegielnia',
+      tier: 1,
+      lvl: 1,
+      posX: 50,
+      posY: 150,
+      sizeX: 200,
+      sizeY: 200,
+    },
+    {
+      name: 'Kuznia',
+      tier: 1,
+      lvl: 1,
+      posX: 280,
+      posY: 20,
+      sizeX: 180,
+      sizeY: 180,
+    },
+    {
+      name: 'Ratusz',
+      tier: 1,
+      lvl: 1,
+      posX: 500,
+      posY: 50,
+      sizeX: 250,
+      sizeY: 250,
+      posLvlX: 30,
+      posLvlY: 40,
+    },
+    {
+      name: 'Wall',
+      tier: 1,
+      lvl: 1,
+      posX: 280,
+      posY: 350,
+      sizeX: 300,
+      sizeY: 300,
+      posLvlX: 30,
+      posLvlY: 40,
+    },
+  ];
 
-  return (
-    <Container>
-      <Warehouse>
-        <BuildingLvL>{warehouseLvl}</BuildingLvL>
-      </Warehouse>
-    </Container>
-  );
+  const BuildingsJSX = Buildings.map(({ name, tier, lvl, posX, posY, sizeY, sizeX, posLvlX, posLvlY }, index) => (
+    <Building
+      key={index}
+      tier={tier}
+      name={name}
+      lvl={lvl}
+      posX={posX}
+      posY={posY}
+      sizeX={sizeX}
+      sizeY={sizeY}
+      posLvlX={posLvlX}
+      posLvlY={posLvlY}
+    />
+  ));
+
+  return <Container>{BuildingsJSX}</Container>;
 };
 
 export default Village;
--- src/Containers/Village/styles.ts
+++ src/Containers/Village/styles.ts
@@ -3,7 +3,8 @@ import palette from 'palette';
 
 export const Container = styled('div')`
   position: absolute;
-  background-image: url('/Arts/Village.jpg');
+  background-image: url('/Arts/MyVillage.jpg');
+  background-size: cover;
   width: 760px;
   height: 760px;
   min-width: 760px;
@@ -12,37 +13,6 @@ export const Container = styled('div')`
   border: 5px solid #5d4a44;
   box-shadow: ${palette.black} 0 0 5px 5px;
   border-radius: 12px;
-`;
-
-export const Warehouse = styled('div')`
-  position: absolute;
-  width: 200px;
-  height: 200px;
-  margin-top: 60px;
-  margin-left: 60px;
-  background-image: url('/Assets/Buildings/Spichlerz-Tier-1-min.png');
-  background-repeat: no-repeat;
-  background-size: 200px;
-  cursor: pointer;
-`;
-
-export const BuildingLvL = styled('div')`
-  position: absolute;
-  width: fit-content;
-  height: fit-content;
-  opacity: 70%;
-  font-weight: 700;
-  font-family: Arial, sans-serif;
-  font-size: 20px;
-  border: 2px solid ${palette.black};
-  background-color: ${palette.dadsRayOfSunshine};
-  border-radius: 4px;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
 `;
 
 export const Building = styled('div')``;
