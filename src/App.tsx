f9792ed098c4e354d44c8398f780ee35d06cae85
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-05-30 14:49:00 +0200

    add useEffect cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,27 @@ import { router } from 'router';
 import 'App.css';
 
 const App = () => {
-  useEffect(() => {
+  const audioListener = () => {
     const audio = document.querySelector('audio');
+    if (audio) {
+      audio.volume = 0.05;
+      audio.play();
+    }
+  };
+
+  useEffect(() => {
     document.addEventListener(
       'click',
-      () => {
-        if (audio) {
-          audio.volume = 0.05;
-          audio.play();
-        }
-      },
-      { once: true }
+      audioListener,
+      { once: true },
     );
+    return () => document.removeEventListener('click', audioListener, false);
   }, []);
 
   return (
     <>
       <audio autoPlay loop>
-        <source src="/Assets/Music/Waterflame%20-%20Glorious%20Morning%20Extended.mp3" type="audio/mpeg" />
+        <source src='/Assets/Music/Waterflame%20-%20Glorious%20Morning%20Extended.mp3' type='audio/mpeg' />
       </audio>
       <RouterProvider router={router} />
     </>
