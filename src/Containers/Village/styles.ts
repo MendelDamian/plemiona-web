833c85c29ceaeec83aa77d36633597eb3ffb1698
VibKkk <240785@edu.p.lodz.pl>
2023-05-31 10:24:58 +0200

    better building positioning

--- src/Containers/Village/styles.ts
+++ src/Containers/Village/styles.ts
@@ -2,17 +2,14 @@ import styled from 'styled-components';
 import palette from 'palette';
 
 export const Container = styled('div')`
-  position: absolute;
+  position: relative;
   background-image: url('/Arts/MyVillage.jpg');
   background-size: cover;
   width: 760px;
   height: 760px;
   min-width: 760px;
   min-height: 760px;
-  margin-right: 800px;
   border: 5px solid #5d4a44;
   box-shadow: ${palette.black} 0 0 5px 5px;
   border-radius: 12px;
 `;
-
-export const Building = styled('div')``;
