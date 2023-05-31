e2235f6b2ed46323167e8dadcd1452224123ede2
VibKkk <240785@edu.p.lodz.pl>
2023-05-31 10:26:15 +0200

    create new container ResourcesTable

---
+++ src/Containers/ResourcesTable/index.tsx
@@ -0,0 +1,7 @@
+import { ResourcesView } from './styles';
+
+const ResourcesTable = () => {
+  return <ResourcesView></ResourcesView>;
+};
+
+export default ResourcesTable;
---
+++ src/Containers/ResourcesTable/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import palette from 'palette';
+
+export const ResourcesView = styled('div')`
+  background-color: ${palette.dadsRayOfSunshine};
+  border: 5px solid #5d4a44;
+  box-shadow: ${palette.black} 0 0 5px 5px;
+  min-width: 760px;
+  border-radius: 12px;
+`;
