e926fad787e1ac80a9e79b6f50863cd3e53b3c19
VibKkk <240785@edu.p.lodz.pl>
2023-06-09 21:07:36 +0200

    change apperances and component logic

--- src/Components/ResourcesComponent/index.tsx
+++ src/Components/ResourcesComponent/index.tsx
@@ -1,34 +1,26 @@
 import { Col, Row } from 'antd';
 
-import { ResourcesImg, ResourcesTag } from './styles';
+import { ResourceImg, ResourceTag } from './styles';
 
 export interface ResourcesProps {
   name: string;
   width?: number;
   height?: number;
-  capacity: number;
+  capacity?: number;
   own: number;
 }
 
-const ResourcesComponent = ({ name, width = 64, height = 64, capacity, own }: ResourcesProps) => {
+const Resource = ({ name, width = 64, height = 64, capacity, own }: ResourcesProps) => {
   return (
-    <Row align={'middle'} justify={'center'} gutter={[10, 0]}>
-      <Col style={{ margin: 'auto' }}>
-        <ResourcesImg
-          style={{
-            width: width,
-            height: height,
-            backgroundImage: `url('/Assets/ResourcesIcons/${name}.png')`,
-          }}
-        />
+    <Row align="middle" justify={'center'} gutter={[10, 0]}>
+      <Col>
+        <ResourceImg type={name} width={width} height={height} />
       </Col>
-      <Col style={{ margin: 'auto' }}>
-        <ResourcesTag>
-          {Math.floor(own)} / {Math.floor(capacity)}
-        </ResourcesTag>
+      <Col>
+        <ResourceTag>{capacity ? `${Math.floor(own)} / ${Math.floor(capacity)}` : Math.floor(own)}</ResourceTag>
       </Col>
     </Row>
   );
 };
 
-export default ResourcesComponent;
+export default Resource;
--- src/Components/ResourcesComponent/styles.ts
+++ src/Components/ResourcesComponent/styles.ts
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
 
-export const ResourcesImg = styled('div')`
+interface ResourceImgProps {
+  type: string;
+  width: number;
+  height: number;
+}
+
+export const ResourceImg = styled('div')<ResourceImgProps>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  background-image: url(${(props) => `/Assets/ResourcesIcons/${props.type}.png`});
   background-size: cover;
 `;
 
-export const ResourcesTag = styled('div')`
-  font-size: 100%;
-  font-weight: 1000;
+export const ResourceTag = styled('div')`
+  font-size: 1.1rem;
+  font-weight: 700;
   color: sandybrown;
   font-family: 'Source Code Pro', sans-serif;
   text-shadow: -1.5px 0 black, 0 1.5px black, 1.5px 0 black, 0 -1.5px black;
