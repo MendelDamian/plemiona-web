0143387a57b17c2a95e1620f0ff33d7c7a8a0b93
VibKkk <240785@edu.p.lodz.pl>
2023-06-11 18:27:09 +0200

    create unit component

---
+++ src/Components/UnitComponent/index.tsx
@@ -0,0 +1,28 @@
+import { Col, Row } from 'antd';
+import { UnitImg, UnitNameTag, UnitTag } from 'Components/UnitComponent/styles';
+import { nameToDisplayName } from 'utils';
+
+export interface UnitProps {
+  name: string;
+  width?: number;
+  height?: number;
+  count: number;
+}
+
+const UnitComponent = ({ name, width = 64, height = 64, count }: UnitProps) => {
+  return (
+    <Row align={'middle'} justify={'center'} gutter={[10, 0]}>
+      <Col style={{ margin: 'auto' }}>
+        <UnitImg type={name} width={width} height={height}></UnitImg>
+      </Col>
+      <Col>
+        <UnitNameTag>{nameToDisplayName(name)} :</UnitNameTag>
+      </Col>
+      <Col style={{ margin: 'auto' }}>
+        <UnitTag>{count}</UnitTag>
+      </Col>
+    </Row>
+  );
+};
+
+export default UnitComponent;
---
+++ src/Components/UnitComponent/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+interface UnitImgProps {
+  type: string;
+  width: number;
+  height: number;
+}
+
+export const UnitImg = styled('div')<UnitImgProps>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  background-image: url(${(props) => `/Assets/UnitsIcons/${props.type}.png`});
+  background-size: cover;
+`;
+
+export const UnitTag = styled('div')`
+  font-size: 1.3rem;
+  font-weight: 700;
+  color: sandybrown;
+  font-family: 'Source Code Pro', sans-serif;
+  text-shadow: -1.5px 0 black, 0 1.5px black, 1.5px 0 black, 0 -1.5px black;
+`;
+
+export const UnitNameTag = styled('div')`
+  font-family: 'Old English Text MT', sans-serif;
+  font-size: 15px;
+  font-weight: 700;
+`;
