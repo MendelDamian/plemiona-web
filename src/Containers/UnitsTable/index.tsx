3e7cf5fd5626c88c85c538dabfa6271514e88a5f
Wiktor Rosiński <104639662+Vittoriio@users.noreply.github.com>
2023-06-12 20:43:16 +0200

    fix to misspellings
    Co-authored-by: Damian Mendel <mendeldamian01@gmail.com>

--- src/Containers/UnitsTable/index.tsx
+++ src/Containers/UnitsTable/index.tsx
@@ -1,17 +1,17 @@
 import { useContext } from 'react';
-
 import { Col, Row } from 'antd';
 
-import GameSessionState, { UnitType } from 'GameSessionContext';
 import UnitComponent from 'Components/UnitComponent';
+
+import GameSessionState, { UnitType } from 'GameSessionContext';
 import { UnitsView } from './styles';
 
-const ResourcesTable = () => {
+const UnitsTable = () => {
   const { gameState } = useContext(GameSessionState);
 
   return (
     <UnitsView>
-      <Row gutter={[40, 0]} justify={'center'} align={'middle'} style={{ marginRight: '10' }}>
+      <Row gutter={[40, 0]} style={{ marginRight: '10' }}>
         {Object.keys(gameState.units).map((unit, idx) => (
           <Col key={idx}>
             <UnitComponent name={unit} count={gameState.units[unit as UnitType].count} />
@@ -22,4 +22,5 @@ const ResourcesTable = () => {
   );
 };
 
-export default ResourcesTable;
+export default UnitsTable;
+
