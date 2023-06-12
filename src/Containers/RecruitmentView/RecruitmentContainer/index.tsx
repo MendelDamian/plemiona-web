bbc0aab1eeed8f2e4bf01c4df8e61d5d1043f759
Wiktor Rosiński <104639662+Vittoriio@users.noreply.github.com>
2023-06-12 20:20:35 +0200

    Update src/Containers/RecruitmentView/RecruitmentContainer/index.tsx
    Co-authored-by: Damian Mendel <mendeldamian01@gmail.com>

--- src/Containers/RecruitmentView/RecruitmentContainer/index.tsx
+++ src/Containers/RecruitmentView/RecruitmentContainer/index.tsx
@@ -26,7 +26,7 @@ const RecruitmentContainer = ({ name, unit, input, setInput }: RecruitmentContai
         <ResourcesComponent name="clay" own={input === 0 ? unit.trainingCost.clay : unit.trainingCost.clay * input} />
       </Col>
       <Col span={4}>
-        <ResourcesComponent name={'iron'} own={input === 0 ? unit.trainingCost.iron : unit.trainingCost.iron * input} />
+        <ResourcesComponent name="iron" own={input === 0 ? unit.trainingCost.iron : unit.trainingCost.iron * input} />
       </Col>
       <Col span={5}>
         <TimeTag>
