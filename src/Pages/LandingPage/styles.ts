39df2c6ea111272a2697aaba13c0811f3d58530e
Blindeen <kuba.szmajda@gmail.com>
2023-06-14 20:17:08 +0200

    change landing page layout

---
+++ src/Pages/LandingPage/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import Button from 'Components/Button';
+import Input from 'Components/Input';
+
+export const StyledButton = styled(Button)`
+  background-color: rgba(255, 252, 159, 30%);
+`;
+
+export const StyledInput = styled(Input)`
+  background-color: rgba(255, 252, 159, 30%);
+`;
